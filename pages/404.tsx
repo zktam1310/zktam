fa69d24b0117b50791fd94cd87232a9415cea774
zktam <zktam@outlook.com>
2022-11-30 02:08:16 +0800

    [feat] init theme and error pages

---
+++ pages/404.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import styles from '../styles/Home.module.css'
+
+export default function NotFound() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/');
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Cliché? Touché!</title>
+        <meta name="description" content="Cliché? Touché!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
