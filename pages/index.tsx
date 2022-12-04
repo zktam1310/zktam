1d7a256efb02b483fef59cb6f5f1e527a1e8c7b4
zktam <zktam@outlook.com>
2022-12-05 02:21:38 +0800

    feat: add youtube embed at homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,29 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const [origin, setOrigin] = useState("");
+
+  useEffect(() => {
+    setOrigin(window.location.origin)
+  }, []);
+
   return (
-    <div className="container">
+    <div className="max-width-container">
       <Head>
         <title>cliché? touché!</title>
         <meta name="description" content="Cliché? Touché!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        It{"'s"} a myth that soundcloud{"'s"} API is so much better than youtube
+      <main className={styles.main}>
+        <span style={{ fontStyle: "italic" }}>life life life</span>
+        <div className={styles.youtube_embed}>
+          <iframe src={`https://www.youtube.com/embed/NwVtIPeYIeQ?autoplay=1&origin=${origin}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" allowFullScreen="True"></iframe>
+        </div>
       </main>
-
     </div>
   )
 }
