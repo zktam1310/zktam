3d44c884c0b5fc818a439abd15d1d60387aea705
zktam <zktam@outlook.com>
2022-12-25 17:05:44 +0800

    feat: add multiple footers displayed randomly

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,46 @@
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
+import Head from 'next/head';
+
+
+function Footer() {
+  const [currentYear, setCurrentyear] = useState("") as any;
+  const [footer, setFooter] = useState("");
+  const initialRun = useRef(true);
+
+  const footers = [
+    "the streets, alleys, corners of the bars",
+    "the front gardens, art parlours, flowers by the highway",
+    "the open smoking area"
+  ];
+
+  useEffect(() => {
+    if (initialRun.current) {
+        (async () => {
+          const current_year = (new Date()).getFullYear();
+          setCurrentyear(current_year);
+          setFooter(footers[Math.floor(Math.random() * footers.length)])
+        })();
+        initialRun.current = false;
+    }
+  }, []);
+
+  return (
+    <footer className="footer">
+      {currentYear} &#169; {footer}
+    </footer>
+  )
+}
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
-  let current_year: any = new Date()
-  current_year = current_year.getFullYear()
   return (
     <>
+      <Head>
+        <title>coffee or tea, I hope you've already had your cup today.</title>
+        <meta name="description" content="offbéat" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="main">{children}</main>
-      <footer className="footer">
-        {current_year} &#169; the streets, alleys, corners of the bars
-      </footer>
+      {Footer()}
     </>
   )
 }
